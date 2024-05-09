a846edecd48ebd52845024cfe647a106caf4d00e
Alessandro Chinchilla <ale030401@gmail.com>
2024-05-09 11:59:41 -0600

    Add: logic to create cards dynamically, add styles and info

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/global.css";
+import "./styles/nav.css";
+import "./styles/header.css";
+import "./styles/main.css";
+import "./styles/footer.css";
+import "./styles/queries.css";
+import renderProjects from "./modules/projects";
+
+renderProjects();
--- src/modules/projects.js
+++ src/modules/projects.js
@@ -6,21 +6,61 @@ export default function renderProjects() {
       name: "Test",
       liveLink: "Link",
       repoLink: "Repo",
-      description: "Lorem",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
       picture: "IMG",
     },
     {
       name: "Test 2",
       liveLink: "Link 2",
       repoLink: "Repo 2",
-      description: "Lorem",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
       picture: "IMG",
     },
   ];
 
   for (let i = 0; i < projects.length; i++) {
+    const projectsList = projects[i];
+
     const projectCard = document.createElement("div");
+    projectCard.classList.add("project-card");
+
+    const imgPlaceholder = document.createElement("div");
+    imgPlaceholder.classList.add("image");
+
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("project-info-container");
+
+    projectCard.append(imgPlaceholder, infoContainer);
+
+    const projectName = document.createElement("h3");
+    projectName.classList.add("project-name");
+    projectName.innerHTML = projectsList.name;
+
+    const linksBox = document.createElement("div");
+    linksBox.classList.add("links-box");
+
+    const repoLink = document.createElement("a");
+    repoLink.href = projectsList.repoLink;
+    repoLink.title = "Github Repo Link";
+    repoLink.innerHTML = "<i class='fa-brands fa-square-github'></i>";
+    const liveLink = document.createElement("a");
+    liveLink.href = projectsList.liveLink;
+    liveLink.title = "Live Site Link";
+    liveLink.innerHTML =
+      " <i class='fa-solid fa-up-right-from-square fa-sm'></i";
+
+    linksBox.append(repoLink, liveLink);
+
+    const projectDescription = document.createElement("p");
+    projectDescription.classList.add("project-description");
+    projectDescription.innerHTML = projectsList.description;
+
+    infoContainer.append(projectName, linksBox, projectDescription);
 
     mainContainer.appendChild(projectCard);
+
+    console.log(projectsList);
   }
 }
