7a6d47d4ac934c9fc55fd097d5137fadcbdc207e
Alessandro Chinchilla <ale030401@gmail.com>
2024-05-14 11:29:35 -0600

    Add: image imports, links and img element

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,29 +1,105 @@
+import battleship from "../images/battleship.png";
+import restaurantPage from "../images/restaurant-page.png";
+import todoList from "../images/todo-list.png";
+import adminDashboard from "../images/admin-dashboard.png";
+import weatherApp from "../images/weather-app.png";
+import ticTacToe from "../images/tic-tac-toe.png";
+import signUpForm from "../images/sign-up-form.png";
+import calculator from "../images/calculator.png";
+import libraryApp from "../images/library-app.png";
+import etchASketch from "../images/etch-a-sketch.png";
+
 export default function renderProjects() {
   const mainContainer = document.getElementById("projects-container");
 
   const projects = [
     {
-      name: "Test",
-      liveLink: "Link",
-      repoLink: "Repo",
+      name: "Restaurant Page/Nitro Coffe",
+      liveLink: "https://chinchilla15.github.io/Restaurant-Page/",
+      repoLink: "https://github.com/Chinchilla15/Restaurant-Page.git",
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
-      picture: "IMG",
+      picture: restaurantPage,
     },
     {
-      name: "Test 2",
-      liveLink: "Link_2",
-      repoLink: "Repo_2",
+      name: "Battleship Game",
+      liveLink: "https://chinchilla15.github.io/battleship/",
+      repoLink: "https://github.com/Chinchilla15/battleship.git",
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
-      picture: "IMG",
+      picture: battleship,
+    },
+    {
+      name: "Todo-list-o",
+      liveLink: "https://chinchilla15.github.io/todo-list/",
+      repoLink: "https://github.com/Chinchilla15/todo-list.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: todoList,
+    },
+    {
+      name: "Admin Dashboard",
+      liveLink: "https://chinchilla15.github.io/admin-dashboard/",
+      repoLink: "https://github.com/Chinchilla15/admin-dashboard.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: adminDashboard,
+    },
+    {
+      name: "Weather App",
+      liveLink: "https://chinchilla15.github.io/weather-app/",
+      repoLink: "https://github.com/Chinchilla15/weather-app.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: weatherApp,
+    },
+    {
+      name: "Tic-Tac-Toe Game",
+      liveLink: "https://chinchilla15.github.io/tic-tac-toe/",
+      repoLink: "https://github.com/Chinchilla15/tic-tac-toe.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: ticTacToe,
+    },
+    {
+      name: "Sign Up Form",
+      liveLink: "https://chinchilla15.github.io/sign-up-form/",
+      repoLink: "https://github.com/Chinchilla15/sign-up-form.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: signUpForm,
+    },
+    {
+      name: "Calculator",
+      liveLink: "https://chinchilla15.github.io/calculator/",
+      repoLink: "https://github.com/Chinchilla15/calculator.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: calculator,
+    },
+    {
+      name: "Library App",
+      liveLink: "https://chinchilla15.github.io/Library/",
+      repoLink: "https://github.com/Chinchilla15/Library.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: libraryApp,
+    },
+    {
+      name: "Etch-a-Sketch",
+      liveLink: "https://chinchilla15.github.io/Etch-a-Sketch/",
+      repoLink: "https://github.com/Chinchilla15/Etch-a-Sketch.git",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Temporibus in animi sed vero velit architecto libero, quidem a.Quos dignissimos placeat exercitationem nam. Doloribus quis, rerumiusto possimus omnis fugiat?",
+      picture: etchASketch,
     },
   ];
 
   function createLink(href, title, iconClass) {
     const link = document.createElement("a");
-    link.href = href || "#";
+    link.href = href;
     link.title = title;
+    link.target = "_blank";
     link.innerHTML = `<i class="${iconClass}"></i>`;
     return link;
   }
@@ -32,8 +108,11 @@ export default function renderProjects() {
     const projectCard = document.createElement("div");
     projectCard.classList.add("project-card");
 
-    const imgPlaceholder = document.createElement("div");
-    imgPlaceholder.classList.add("image");
+    const img = document.createElement("img");
+    img.classList.add("project-image");
+    img.alt = project.name;
+    img.src = project.picture;
+    img.loading = "lazy";
 
     const infoContainer = document.createElement("div");
     infoContainer.classList.add("project-info-container");
@@ -63,7 +142,7 @@ export default function renderProjects() {
     projectDescription.textContent = project.description;
 
     infoContainer.append(projectName, linksBox, projectDescription);
-    projectCard.append(imgPlaceholder, infoContainer);
+    projectCard.append(img, infoContainer);
 
     return projectCard;
   }
