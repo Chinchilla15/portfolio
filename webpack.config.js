8ca0f830e738cb70677038a15c17a76903788760
Alessandro Chinchilla <ale030401@gmail.com>
2024-05-14 11:31:34 -0600

    Update webpack config for img loading

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+      },
     ],
   },
   devServer: {
