a454577ac32fe7bcbbc303fc2948117e13bdf645
Alessandro Chinchilla <ale030401@gmail.com>
2024-10-07 20:35:22 -0600

    Feat: add hamburger menu for small screens

--- src/index.js
+++ src/index.js
@@ -6,4 +6,22 @@ import "./styles/footer.css";
 import "./styles/queries.css";
 import renderProjects from "./modules/projects";
 
+const burgerMenu = document.getElementById("burger-menu");
+const menu = document.getElementById("menu");
+const mobileMenuLinks = document.querySelectorAll(".side-nav-list-item");
+
+console.log(mobileMenuLinks);
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("close");
+  menu.classList.toggle("overlay");
+});
+
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.toggle("close");
+    menu.classList.toggle("overlay");
+  });
+});
+
 renderProjects();
